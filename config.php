bfb7a4ce192fbe80acd1bf4b1bd5b5c2767073d8
Rami23dz <rbelabed2@gmail.com>
2024-08-12 05:00:18 +0200

    portfolio

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "portfolio";
+
+if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+{
+
+	die("faild to connect!");
+}
