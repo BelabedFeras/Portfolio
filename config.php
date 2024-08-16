3f7706bce3c9bdff7a9fcec23c063d65080cf7fd
BelabedFeras <belabedferas@gmail.com>
2024-08-16 18:59:25 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "portfolio";
+$dbhost = "ep-gentle-wave-a27x9yzr-pooler.eu-central-1.aws.neon.tech";
+$dbuser = "default";
+$dbpass = "4vp1LQhwJljI";
+$dbname = "verceldb";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
